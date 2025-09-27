bdf6ff0024a2fd4ab325530969543c952a97d1a4
pero4l <lilpero03@gmail.com>
2025-09-28 00:12:00 +0100

    writing login middleware

---
+++ middleware/loginMiddleware.js
@@ -0,0 +1,33 @@
+const {readDb} = require('../utils/dbOperation')
+const bcrypt = require('bcrypt')
+
+async function loginMiddleware(req, res, next){
+    const {email, password} = req.body
+
+    const data = readDb()
+    const existUser = data['users'].find((u) => u.email === email)
+
+    if (!userExist) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid credentials"
+        });
+    }
+
+    const passMatch = await bcrypt.compare(password, existUser.password)
+     if (!passMatch) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid credentials"
+        });
+    }
+
+    req.user = passMatch
+    req.data = userExist
+    next()
+}
+
+
+module.exports = {
+    loginMiddleware
+}
