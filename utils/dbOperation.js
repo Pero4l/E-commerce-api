5c997292765397908d9a9b2bfce2ea1f36560836
pero4l <lilpero03@gmail.com>
2025-09-27 22:20:13 +0100

    Added the files needed and wrote the readDb function

---
+++ utils/dbOperation.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const filePath = './data.json'
+
+function readDb(){
+    try{
+        if(!fs.existsSync(filePath)) return {products: [], orders: [], users: []}
+        const data = fs.readFileSync(filePath, "utf8");
+        return JSON.parse(data)
+    } catch (err){
+        console.log("Error reading file:", err);
+        return {products: [], orders: [], users: []};
+        
+    }
+}
+
