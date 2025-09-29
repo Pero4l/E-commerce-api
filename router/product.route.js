b752a9938c008526ee90ff49dda2ce3a4d6dc7b2
pero4l <lilpero03@gmail.com>
2025-09-29 01:51:28 +0100

    routed it and want to tested it

--- router/product.route.js
+++ router/product.route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const {addProduct, editProduct} = require('../controllers/productController')
+const {addProduct, editProduct, seeAllProducts} = require('../controllers/productController')
 
 const {authMiddleware} = require('../middleware/authUserMiddleware')
 const {isAdmin} = require('../middleware/authPost')
@@ -9,6 +9,7 @@ const {isAdmin} = require('../middleware/authPost')
 
 router.post('/add',authMiddleware, isAdmin, addProduct)
 router.patch('/edit', authMiddleware, isAdmin, editProduct)
+router.get('/products', authMiddleware, seeAllProducts)
 
 
 module.exports = router
