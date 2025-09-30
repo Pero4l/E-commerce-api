927f0c18eddaf4386c1477a66336b06c797de097
pero4l <lilpero03@gmail.com>
2025-09-30 12:41:02 +0100

    routed it and want to tested it

--- router/product.route.js
+++ router/product.route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const {addProduct, editProduct, seeAllProducts, seeSingleProduct, buyProduct, seeAllOrders, cart, processOrder} = require('../controllers/productController')
+const {addProduct, editProduct, seeAllProducts, seeSingleProduct, buyProduct, seeAllOrders, cart, processOrder, searchOrder} = require('../controllers/productController')
 
 const {authMiddleware} = require('../middleware/authUserMiddleware')
 const {isAdmin} = require('../middleware/authPost')
@@ -14,6 +14,7 @@ router.post('/buy', authMiddleware, buyProduct)
 router.get('/allorders', authMiddleware, isAdmin, seeAllOrders)
 router.get('/cart', authMiddleware, cart)
 router.post('/processorder', authMiddleware, isAdmin, processOrder)
+router.post('/search', authMiddleware, searchOrder)
 router.get('/:id', authMiddleware, seeSingleProduct)
 
 
